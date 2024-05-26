2231abfe5087509476f37e4634390c3c083241a0
Braeden <flamemasternxf@gmail.com>
2024-05-26 12:50:04 -0400

    feat(rofi): Display Wallpaper Previews

--- .config/rofi/scripts/background.sh
+++ .config/rofi/scripts/background.sh
@@ -1,19 +1,34 @@
 #!/usr/bin/env sh
 
-# cd ~/wallpapers
+# Get and List  Wallpapers
+wallpapers=($(find "$HOME/wallpapers" -type f -iname \*.png))
+listWallpapers() {
+    sorted=($(printf '%s\n' "${wallpapers[@]}" | sort))
+    for path in "${sorted[@]}"; do
+        name=$(basename "$path")
+        printf "%s\x00icon\x1f%s\n" "$(echo "$name" | cut -d. -f1)" "$path"
+    done
+}
 
 # Run Rofi to Select a new Wallpaper
-chosen=$(ls ~/wallpapers/ | rofi -dmenu -selected-row 0)
+wallpaper=$(listWallpapers | rofi -i -show drun -dmenu)
+
+# Get the Index of the Wallpaper (there could be an easier way to do this?)
+index=-1
+for i in "${!wallpapers[@]}"; do
+    filename=$(basename "${wallpapers[$i]}")
+    if [[ "$filename" == "$wallpaper"* ]]; then
+        index=$i
+        break
+    fi
+done
 
 # Swap the Wallpaper and Theme
-swww img ~/wallpapers/$chosen 
-wallust run ~/wallpapers/$chosen
+swww img "${wallpapers[$index]}" 
+wallust run "${wallpapers[$index]}"
 
 # Refresh Waybar
 killall -SIGUSR2 waybar > /dev/null
 
 # Copy the Wallpaper to Rofi
-cp ~/wallpapers/$chosen ~/dotfiles/.config/rofi/.wallpaper
- 
-# Regex Used to Hide File Extensions, removed for practical purposes 
-# | sed 's/\.[^.]*$//'
+cp "${wallpapers[$index]}" ~/dotfiles/.config/rofi/.wallpaper
